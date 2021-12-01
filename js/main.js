f2ba0f21db52ae4c41ed1e73685284d7d2812ad0
yuni0105 <93234787+yuni0105@users.noreply.github.com>
2021-12-01 18:01:09 +0900

    Revert "21-12-01"
    This reverts commit fc00f5293e1182f6d385cfd70357a2445aca8a86.

--- js/main.js
+++ js/main.js
@@ -105,49 +105,47 @@ $(document).ready(function () {
 
 
 //review
-$(function(){
-    function left() {
+function review_left() {
 
-        $('.review_box li:last').prependTo('.review_box');
-        $('.review_box').css('margin-left', -373);
-        $('.review_box').stop().animate({ marginLeft: 0 }, 800);
-    }
-    
-    
-    
-    function right() {
-    
-        $('.review_box').stop().animate({ marginLeft: -373 }, 800,function () {
-            $('.review_box li:first').appendTo('.review_box');
-            $('.review_box').css({ marginLeft: 0 });
-        });
-    
-    }
-    
-    
-    
-    function slide() {
-    
-        $('.review_box').stop().animate({ marginLeft: -373 },800, function () {
-            $('.review_box li:first').appendTo('.review_box');
-            $('.review_box').css({ marginLeft: 0 });
-        });
-    
-    }
-    
-    
-    
-    setInterval(slide, 5000);
-    
-    
-    
-    $('.review_left').click(function(){
-        left();
+    $('.review_box li:last').prependTo('.review_box');
+    $('.review_box').css('margin-left', -355);
+    $('.review_box').stop().animate({ marginLeft: 0 }, 800);
+}
+
+
+
+function review_right() {
+
+    $('.review_box').stop().animate({ marginLeft: -350 }, function () {
+        $('.review_box li:first').appendTo('.review_box');
+        $('.review_box').css({ marginLeft: 0 });
     });
-    
-    
-    
-    $('.review_right').click(function(){
-        right();
+
+}
+
+
+
+function slide() {
+
+    $('.review_box').stop().animate({ marginLeft: -350 }, function () {
+        $('.review_box li:first').appendTo('.review_box');
+        $('.review_box').css({ marginLeft: 0 });
     });
+
+}
+
+
+
+setInterval(slide, 3000);
+
+
+
+$('.review_left').click(function () {
+    review_left();
+});
+
+
+
+$('.review_right').click(function () {
+    review_right();
 });
